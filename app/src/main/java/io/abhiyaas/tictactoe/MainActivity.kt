b02582903e8f8355bc7be106460744c41a8fa166
davidchhetri <david_chhetri@yahoo.com>
2024-04-15 09:14:29 -0400

    implemented player's moves

--- app/src/main/java/io/abhiyaas/tictactoe/MainActivity.kt
+++ app/src/main/java/io/abhiyaas/tictactoe/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -26,8 +27,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -69,12 +72,28 @@ fun MainScreen() {
         )
     }
 
+
+    val onTap: (Offset) -> Unit = {
+        if (playerTurn.value ) {
+            val x = (it.x / 333).toInt()
+            val y = (it.y / 333).toInt()
+            val posInMoves = y * 3 + x
+            if (moves[posInMoves] == null) {
+                moves[posInMoves] = true
+                playerTurn.value = false
+            }
+        }
+
+    }
+
+
+
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
         Text(text = "TicTacToe", fontSize = 30.sp, modifier = Modifier.padding(16.dp))
 
         Header(playerTurn.value)
 
-        Board(moves)
+        Board(moves, onTap)
 
     }
 
@@ -122,12 +141,17 @@ fun Header(playerTurn: Boolean) {
 }
 
 @Composable
-fun Board(moves: List<Boolean?>) {
+fun Board(moves: List<Boolean?>,onTap: (Offset) -> Unit ) {
     Box(
         modifier = Modifier
             .aspectRatio(1f)
             .padding(32.dp)
             .background(Color.LightGray)
+            .pointerInput(Unit) {
+                detectTapGestures(
+                    onTap = onTap
+                )
+            }
 
 
     ) {
