2056b4ca50dbdd267b288e60cb3ce7ef99978db6
davidchhetri <david_chhetri@yahoo.com>
2024-04-15 08:03:36 -0400

    barebone before implementing header

--- app/src/main/java/io/abhiyaas/tictactoe/MainActivity.kt
+++ app/src/main/java/io/abhiyaas/tictactoe/MainActivity.kt
@@ -8,6 +8,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import io.abhiyaas.tictactoe.ui.theme.TicTacToeTheme
@@ -22,10 +25,48 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    TTTScreen()
                 }
             }
         }
     }
 }
 
+@Composable
+fun TTTScreen() {
+    val playerTurn = remember { mutableStateOf(true) }
+
+    //true means player move, false means AI move, null means no move
+    val moves = remember {
+        mutableStateListOf<Boolean?>(
+            true,
+            null,
+            false,
+            null,
+            true,
+            null,
+            false,
+            null,
+            null
+        )
+    }
+
+    Header(playerTurn.value)
+
+    Board(moves)
+
+
+
+}
+
+@Composable
+fun Header(playerTurn: Boolean) {
+
+
+}
+
+@Composable
+fun Board(moves: List<Boolean?>){
+
+
+}
