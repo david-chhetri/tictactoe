b5d335ef2b0433fff9836ee6697cae0b4f1f8d0d
davidchhetri <david_chhetri@yahoo.com>
2024-04-15 08:09:24 -0400

    header for player and ai implemented

--- app/src/main/java/io/abhiyaas/tictactoe/MainActivity.kt
+++ app/src/main/java/io/abhiyaas/tictactoe/MainActivity.kt
@@ -3,7 +3,14 @@ package io.abhiyaas.tictactoe
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -11,8 +18,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import io.abhiyaas.tictactoe.ui.theme.TicTacToeTheme
 
 class MainActivity : ComponentActivity() {
@@ -61,6 +71,40 @@ fun TTTScreen() {
 
 @Composable
 fun Header(playerTurn: Boolean) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround
+    ) {
+        val playerBoxColor = if (playerTurn) Color.Blue else Color.LightGray
+        val aiBoxColor = if (playerTurn) Color.LightGray else Color.Red
+        //player box
+        Box(
+            modifier = Modifier
+                .width(100.dp)
+                .background(playerBoxColor)
+        ) {
+            Text(
+                text = "Player", modifier = Modifier
+                    .padding(8.dp)
+                    .align(Alignment.Center)
+            )
+        }
+        Spacer(modifier = Modifier.width(50.dp))
+
+        //AI box
+        Box(
+            modifier = Modifier
+                .width(100.dp)
+                .background(aiBoxColor)
+        ) {
+            Text(
+                text = "AI", modifier = Modifier
+                    .padding(8.dp)
+                    .align(Alignment.Center)
+            )
+        }
+
+    }
 
 
 }
